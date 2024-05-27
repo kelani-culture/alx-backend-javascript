d6e36e1490d277c18d380cdb73cb59739f659d84
Null <kelanidarasimi9@gmail.com>
2024-05-28 00:36:41 +0100

    task 12 redid

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+/** @format */
+
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(obj) {
+      return Object.keys(obj).length;
+    },
+  };
+}
