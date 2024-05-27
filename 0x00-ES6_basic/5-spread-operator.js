7d783d5dbc7afcb656761411f1d4c23088b742bf
Null <kelanidarasimi9@gmail.com>
2024-05-27 22:58:23 +0100

    task 5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,6 +1,3 @@
-/** @format */
-
 export default function concatArrays(array1, array2, string) {
-  const arr = array1.concat(array2).concat(...string);
-  return arr;
+  return array1.concat(...array2).concat(...string);
 }
