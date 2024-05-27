2f28e59ab2b4d39d4e23bd9036f572414d5bc166
Null <kelanidarasimi9@gmail.com>
2024-05-27 22:31:21 +0100

    task 2

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import getNeighborhoodsList from "./2-arrow.js";
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood("Noe Valley");
+console.log(res);
