68ae0a042f711efe93e29ad592c3e3a7ae1263b2
Null <kelanidarasimi9@gmail.com>
2024-05-27 21:51:48 +0100

    task 0 redid

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,20 +1,21 @@
 /**
  * a simple java scritp task to modify a function
+ *
+ * @format
  */
 
-export const taskFirst = function () {
-    var task = 'I prefer const when I can';
-    return task;
-};
-
+export function taskFirst() {
+  const task = "I prefer const when I can.";
+  return task;
+}
 
 export function getLast() {
   return ' is okay';
 }
 
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-export let taskNext = function () {
-    var combination = 'But sometimes let';
-    combination += getLast();
-    return combination
-};
+  return combination;
+}
