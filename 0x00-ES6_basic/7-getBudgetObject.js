395cdef38dd7792c254b78c9781ef6eae5246483
Null <kelanidarasimi9@gmail.com>
2024-05-27 23:28:25 +0100

    task 7

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,6 @@
+/** @format */
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = { income, gdp, capita };
+  return budget;
+}
---
+++ 0x00-ES6_basic/7-main.js
@@ -0,0 +1,5 @@
+/** @format */
+
+import getBudgetObject from "./7-getBudgetObject.js";
+
+console.log(getBudgetObject(400, 700, 900));
