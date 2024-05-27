73f68e278f865e206c4bdc2b93d1ae6c60da5b93
Null <kelanidarasimi9@gmail.com>
2024-05-27 21:44:51 +0100

    task 0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,20 @@
+/**
+ * a simple java scritp task to modify a function
+ */
+
+export const taskFirst = function () {
+    var task = 'I prefer const when I can';
+    return task;
+};
+
+
+export function getLast() {
+  return ' is okay';
+}
+
+
+export let taskNext = function () {
+    var combination = 'But sometimes let';
+    combination += getLast();
+    return combination
+};
