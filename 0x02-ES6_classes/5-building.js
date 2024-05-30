f8ee81755d32cccb12d54066d2af30efc5e82f78
Null <kelanidarasimi9@gmail.com>
2024-05-31 00:08:04 +0100

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,32 @@
 export default class Building {
-  constructor (sqft) {
-    this._sqft = sqft;
+  constructor(sqft) {
+    this._sqft = this._validateNumber(sqft, 'Square feet');
+
+    // Check if the subclass has implemented evacuationWarningMessage
+    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage && this.constructor.name !== 'Building') {
+      this.evacuationWarningMessage();
+    }
   }
 
-  get sqft(){
+  // Getter for sqft
+  get sqft() {
     return this._sqft;
   }
 
-  set sqft(value){
-    return this.sqft = value;
+  // Setter for sqft
+  set sqft(value) {
+    this._sqft = this._validateNumber(value, 'Square feet');
   }
 
+  // Validation method for numbers
+  _validateNumber(value, attributeName) {
+    if (typeof value !== 'number' || value <= 0) {
+      throw new TypeError(`${attributeName} must be a positive number.`);
+    }
+    return value;
+  }
+
+  // Abstract method
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
