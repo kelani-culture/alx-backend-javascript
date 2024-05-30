7ab83789ca1c21c735241c806eb3b3f4c71a4243
Null <kelanidarasimi9@gmail.com>
2024-05-30 15:30:35 +0100

    task 8

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,9 @@
+/** @format */
+
+export default function divideFunction(numerator, denominator) {
+
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
