7ebc19340c84e25f91fe2e62479ddbd9e3ae969a
Null <kelanidarasimi9@gmail.com>
2024-05-30 14:37:24 +0100

    task 3 redid

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,14 +1,5 @@
 /** @format */
 
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve) => {
-    // Simulate an asynchronous operation (like user creation)
-    resolve({ firstName: firstName, lastName: lastName });
-  })
-    .then((user) => {
-      console.log(user);
-    })
-    .catch(() => {
-      console.error("Signup system offline");
-    });
+  return Promise.resolve({ firstName, lastName });
 }
