2b93fa240b428c74f881ce5857f603458643eaf2
Null <kelanidarasimi9@gmail.com>
2024-05-30 22:43:10 +0100

    task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,57 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = this._validateString(name, 'Name');
+    this._length = this._validateNumber(length, 'Length');
+    this._students = this._validateArrayOfStrings(students, 'Students');
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = this._validateString(value, 'Name');
+  }
+
+  // Getter and Setter for length
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = this._validateNumber(value, 'Length');
+  }
+
+  // Getter and Setter for students
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._students = this._validateArrayOfStrings(value, 'Students');
+  }
+
+  // Helper methods to validate types
+  _validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
+    }
+    return value;
+  }
+
+  _validateNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attributeName} must be a number`);
+    }
+    return value;
+  }
+
+  _validateArrayOfStrings(value, attributeName) {
+    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
+      throw new TypeError(`${attributeName} must be an array of strings`);
+    }
+    return value;
+  }
+}
+
