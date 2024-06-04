d58945ff2c415df60efc68867f53a4655234ac28
Null <kelanidarasimi9@gmail.com>
2024-06-04 12:39:46 +0100

    task 0 redid

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
-  const list_dict = [
-   {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-   {id: 2, firstName: 'James', location: 'Columbia'},
-   {id: 3, firstName: 'Serena', location: 'San Francisco'}
+  const listDict = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  return list_dict;
+  return listDict;
 }
