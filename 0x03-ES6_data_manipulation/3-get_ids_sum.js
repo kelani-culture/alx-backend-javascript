d331195a7cf59eb6f44d945ac78b3615e300534b
Null <kelanidarasimi9@gmail.com>
2024-06-04 18:44:37 +0100

    task 3 redid

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(array) {
-  if (!Array.isArray(array)) { return []; };
+  if (!Array.isArray(array)) { return []; }
   return array.reduce((total, data) => total + data.id, 0);
 }
