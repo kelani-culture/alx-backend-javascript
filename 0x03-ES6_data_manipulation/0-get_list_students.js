d4eb0dfbbef7ab5b60c87d4744e50be7e54a874e
Null <kelanidarasimi9@gmail.com>
2024-06-04 12:34:34 +0100

    create an array of object

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const list_dict = [
+   {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+   {id: 2, firstName: 'James', location: 'Columbia'},
+   {id: 3, firstName: 'Serena', location: 'San Francisco'}
+  ];
+  return list_dict;
+}
