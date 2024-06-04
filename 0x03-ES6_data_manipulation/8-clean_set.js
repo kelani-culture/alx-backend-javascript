60e05cebbfc06a0a2773294c41eb26ac8b9e406f
Null <kelanidarasimi9@gmail.com>
2024-06-04 23:00:27 +0100

    task 89

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
   let result = '';
+  if (!startString) {
+    return;
+  }
   set.forEach((value) => {
     if (value.startsWith(startString)) {
       // Append the rest of the string after startString
