cdc7b46056667eb0405cb444c07d3cace4101a6d
Null <kelanidarasimi9@gmail.com>
2024-06-05 22:41:11 +0100

    task 7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,8 +1,10 @@
+/** @format */
+
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
-};
+}
 
 interface TeacherInterface {
   workFromHome(): string;
@@ -10,56 +12,65 @@ interface TeacherInterface {
   workTeacherTasks(): string;
 }
 
-
 class Director implements DirectorInterface {
   workFromHome(): string {
-    return 'Working from home';
+    return "Working from home";
   }
 
   getCoffeeBreak(): string {
-    return 'Getting a coffee break';
+    return "Getting a coffee break";
   }
 
   workDirectorTasks(): string {
-    return 'Getting to director tasks';
+    return "Getting to director tasks";
   }
 }
 
-class Teacher implements TeacherInterface{
+class Teacher implements TeacherInterface {
   workFromHome(): string {
-    return 'Cannot work from home';
+    return "Cannot work from home";
   }
 
   getCoffeBreak(): string {
-    return 'Cannot have a break';
+    return "Cannot have a break";
   }
 
   workTeacherTasks(): string {
-    return 'Getting to work';
+    return "Getting to work";
   }
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
+  if (typeof salary === "number" && salary < 500) {
     return new Teacher();
   }
 
-  return new Director()
+  return new Director();
 }
-
 
 const employee1 = createEmployee(300);
 
-if (employee1 instanceof Teacher)  {
-  console.log(employee1.workFromHome());  
-  console.log(employee1.getCoffeBreak());  
-  console.log(employee1.workTeacherTasks()); 
+if (employee1 instanceof Teacher) {
+  console.log(employee1.workFromHome());
+  console.log(employee1.getCoffeBreak());
+  console.log(employee1.workTeacherTasks());
 }
 
 const employee2 = createEmployee(1000);
 
 if (employee2 instanceof Director) {
-  console.log(employee2.workFromHome()); 
+  console.log(employee2.workFromHome());
   console.log(employee2.getCoffeeBreak());
   console.log(employee2.workDirectorTasks());
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks() !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks()
 }
