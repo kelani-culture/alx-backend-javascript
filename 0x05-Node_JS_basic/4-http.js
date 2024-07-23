e8e7f72678831f28fc18ca921657970444101c40
Null <kelanidarasimi9@gmail.com>
2024-07-23 18:26:24 +0100

    task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+// 4-http.js
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app;
