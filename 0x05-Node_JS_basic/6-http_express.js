ce5edbac62718a541d5a8307879d12df002fe519
Null <kelanidarasimi9@gmail.com>
2024-07-23 18:31:56 +0100

    task 6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+// 6-http_express.js
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app;
